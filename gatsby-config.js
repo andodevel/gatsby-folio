98adc245023a57c5981b839cac9e21c2e5ba11b7
An Do <ando.devel@gmail.com>
2020-03-29 00:50:54 +0700

    Disable sitemap plugin due to failed netlify build

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sitemap`,
+    // `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     `gatsby-plugin-offline`,
     {
